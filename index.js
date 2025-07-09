a4904709e64d2391c363fb57df793b37799d3052
mdsharuk <mdsharuk1011@gmail.com>
2025-07-09 11:08:37 +0600

    Initial project upload

---
+++ index.js
@@ -0,0 +1,21 @@
+/**
+ * Title: Uptime Monitoring API
+ * Description: A RESTful API for monitoring the uptime of various services.
+ * @author Muhammad Shahrukh
+ * Version: 1.0.0S
+ * @date 2025-08-07
+ */
+
+// Dependencies
+const http = require("http");
+
+// App object - module scaffolding
+const app = {};
+
+// Configuration
+app.config = {
+  port: 3000,
+};
+
+// Create server
+app.createServer = () => {};
